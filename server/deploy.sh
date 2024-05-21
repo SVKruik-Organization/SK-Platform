35eb7441420a9cb2e2e10610bcb12d280d39586c
Stefan Kruik <s.kruik@icloud.com>
2024-05-21 21:57:08 +0200

    Deployment 2

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-cd ../frontend
+cd ..
+git reset --hard
+git pull
+
+cd frontend
+npm install
 npm run build
 echo "Build complete"
+
 cd ../server
+npm install
 rm -rf dist
 mkdir -p dist
 mv ../frontend/dist/* dist/
-echo "Dist deployment complete. Reloading server."
+echo "Migration complete"
+
+cd ../frontend
+rm -rf dist
+
+sudo systemctl restart bot-website.service
+echo "Deployment complete. Reloaded server."
