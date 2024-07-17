1b4b01add52bab9c7c267316a9e59020b8b15573
Stefan Kruik <s.kruik@icloud.com>
2024-07-17 19:54:51 +0200

    Legal Docs

--- docs/src/utils/middleware.ts
+++ docs/src/utils/middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export function deploymentAuthentication(req: Request, res: Response, next: NextFunction) {
-    const password = req.headers.authorization;
+    const password: string | undefined = req.headers.authorization;
     if (!password || password.split(" ")[1] !== process.env.DEPLOYMENT_KEY) return res.sendStatus(401);
     next();
 }
