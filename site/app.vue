e60ecec29f9cdbf937999e45f0846d45b4ed315b
Stefan Kruik <s.kruik@icloud.com>
2025-02-10 21:10:55 +0100

    Update app.vue

--- site/app.vue
+++ site/app.vue
@@ -3,13 +3,13 @@ import "~/assets/css/base.css";
 </script>
 
 <template>
-    <h1>SK Horizon</h1>
+    <h1>Stefan Kruik</h1>
+    <p>Thanks for visiting my page!</p>
     <p>This website is WIP. It will be a sitemap/overview for all my projects. For now you can visit the following
         websites:</p>
     <ul>
-        <li><a href="https://docs.stefankruik.com" target="_blank">SK Docs</a></li>
-        <li><a href="https://metrics.stefankruik.com" target="_blank">SK Metrics</a></li>
-        <li><a href="https://stefankruik.com" target="_blank">Portfolio</a></li>
+        <li><a href="https://docs.stefankruik.com" target="_blank">SK Docs (main priority)</a></li>
+        <li><a href="https://stefankruik.com" target="_blank">Portfolio (also WIP)</a></li>
     </ul>
 </template>
 
